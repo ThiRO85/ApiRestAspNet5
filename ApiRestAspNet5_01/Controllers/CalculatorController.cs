7aba71cafae6e407a6342ab36f9000dc6b218169
ThiRO85 <thgr85@gmail.com>
2022-01-25 14:49:13 -0300

    Implementing Calculator

---
+++ ApiRestAspNet5_01/Controllers/CalculatorController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+
+namespace ApiRestAspNet5_01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Ivalid input!");
+        }
+
+        [HttpGet("squareRoot/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var sqrt = Math.Sqrt(ConvertToDouble(number));
+                return Ok(sqrt.ToString());
+            }
+            return BadRequest("Ivalid input!");
+        }
+
+        [HttpGet("power/{firstNumber}/{secondNumber}")]
+        public IActionResult Power(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var pow = Math.Pow(ConvertToDouble(firstNumber), ConvertToDouble(secondNumber));
+                return Ok(pow.ToString());
+            }
+            return BadRequest("Ivalid input!");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber, NumberStyles.Any, 
+                NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private double ConvertToDouble(string strNumber)
+        {
+            double doubleValue;
+            if (double.TryParse(strNumber, out doubleValue))
+            {
+                return doubleValue;
+            }
+            return 0.0;
+        }
+    }
+}
