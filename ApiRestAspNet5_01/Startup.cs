89c25c5b356a5e111f6def231dc8076a76b22688
ThiRO85 <thgr85@gmail.com>
2022-01-31 20:38:33 -0300

    Default Swagger

--- ApiRestAspNet5_01/Startup.cs
+++ ApiRestAspNet5_01/Startup.cs
@@ -6,11 +6,14 @@ using ApiRestAspNet5_01.Repository.Implementations;
 using ApiRestAspNet5_01.Services.Implementations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace ApiRestAspNet5_01
 {
@@ -49,6 +52,20 @@ namespace ApiRestAspNet5_01
             
             services.AddApiVersioning();
 
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo {
+                    Title = "REST API's from 0 to Azure with ASP.NET Core 5 and Docker",
+                    Version = "v1",
+                    Description = "API RESTful developed in course 'REST API's from 0 to Azure with ASP.NET Core 5 and Docker'",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Thiago Oliveira",
+                        Url = new Uri("https://github.com/ThiRO85")
+                    }
+                    });
+            });
+
             services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
             services.AddScoped<IPersonService, PersonServiceImplementation>();
             services.AddScoped<IBookService, BookServiceImplementation>();
@@ -67,6 +84,17 @@ namespace ApiRestAspNet5_01
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "REST API's from 0 to Azure with ASP.NET Core 5 and Docker - v1");
+            });
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
 
             app.UseAuthorization();
 
